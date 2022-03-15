6e497e1d91c8f886b0d82d5acb1169af78b01552
miqbalnugraha <iqbalturmudzi@gmail.com>
2022-03-15 20:15:17 +0700

    string

--- string.go
+++ string.go
@@ -6,4 +6,8 @@ func main() {
 	fmt.Println("Eko")
 	fmt.Println("Iqbal Nugraha")
 	fmt.Println("Muhammad Iqbal Nugraha")
+
+	fmt.Println(len("Eko"))
+	fmt.Println("Iqbal Nugraha"[0])
+	fmt.Println("Muhammad Iqbal Nugraha"[1])
 }
